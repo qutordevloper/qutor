f29b92170cdb3127981372b72b8b9badbedee8f8
qutordevloper <qutordevolepor@gmail.com>
2025-07-30 17:46:48 +0530

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,65 +1,42 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-  import { getauth, createuserwithemailandpassword, signinwithemailandpassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-  import { getFirestore, setDoc, Doc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAPhHWUUicDAgCg_Myefi5J4eTCQRNGnUM",
-    authDomain: "qutor-fdd88.firebaseapp.com",
-    projectId: "qutor-fdd88",
-    storageBucket: "qutor-fdd88.firebasestorage.app",
-    messagingSenderId: "433760997594",
-    appId: "1:433760997594:web:4592dbf1fcf9bc6011d5e3",
-    measurementId: "G-7X1CK9JXJP"
-  };
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPhHWUUicDAgCg_Myefi5J4eTCQRNGnUM",
+  authDomain: "qutor-fdd88.firebaseapp.com",
+  projectId: "qutor-fdd88",
+  storageBucket: "qutor-fdd88.firebasestorage.app",
+  messagingSenderId: "433760997594",
+  appId: "1:433760997594:web:4592dbf1fcf9bc6011d5e3",
+  measurementId: "G-7X1CK9JXJP"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  function showmessage(message, divid) {
-    var messageDiv = document.getElementById(divid);
-    messageDiv.style.display = "block";
-    messageDiv.style.opacity
-    messageDiv.innerHTML = message;
-    setTimeout(function() {
-        messageDiv.style.opacity = "0";
-    }, 5000);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Show message helper
+function showMessage(message, divId) {
+  const messageDiv = document.getElementById(divId);
+  messageDiv.style.display = "block";
+  messageDiv.innerHTML = message;
+  messageDiv.style.opacity = "1";
+  setTimeout(() => {
+    messageDiv.style.opacity = "0";
+  }, 5000);
 }
 
-const signup=document.getElementById("submitSignUp");
-signup.addEventListener("click", (Event) => {
-    const email = document.getElementById("remail").value;
-    const password = document.getElementById("rPassword").value;
-    const firstname = document.getElementById("rFirstName").value;
-    const lastname = document.getElementById("rLastName").value;
-    const auth = getAuth();
-    const firestore = getFirestore();
+// Sign up event listener
+document.getElementById("submitSignUp").addEventListener("click", (event) => {
+  event.preventDefault();
 
-    createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-        const user = userCredential.user;
-        setDoc(doc(getFirestore(), "users", user.uid), {
-            first: firstname,
-            last: lastname,
-            email: email
-        });
-        showmessage("Account Created Successfully", "signUpMessage");
-        const docref = doc(db, "users", user.uid);
-        setDoc(docref, userdata).then(() => {
-            window.location.href = "index.html";
-        }).catch((error) => {
-            console.error("Error writing document: ", error);
-        });
+  const email = document.getElementById("remail").value;
+  const password = document.getElementById("rPassword").value;
+  const firstname = document.getElementById("rFirstName").value;
+  const lastname = document.getElementById("rLastName").value;
 
-    }).catch((error) => {
-        const errorcode = error.code;
-        if (errorcode === "auth/email-already-in-use") {
-            showmessage("Email Already in Use!!!", "signUpMessage");
-        }
-        else {
-            showmessage("unable to create account!!", "signUpMessage");
-        }
-    });
-});
+  createUserWithEmailAndPassword
