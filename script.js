7adaa7db3ed0227f7cf596e2f8bffd4b815d2191
qutordevloper <qutordevolepor@gmail.com>
2025-07-30 17:52:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const signUpButton=document.getElementById('signUpButton');
-const signInButton=document.getElementById('signInButton');
-const signInForm=document.getElementById('signIn');
-const signUpForm=document.getElementById('signup');
+const signUpButton = document.getElementById('signUpButton');
+const signInButton = document.getElementById('signInButt');
+const signInForm = document.getElementById('signIn');
+const signUpForm = document.getElementById('signup');
 
-signUpButton.addEventListener('click',function(){
-    signInForm.style.display="none";
-    signUpForm.style.display="block";
-})
-signInButton.addEventListener('click', function(){
-    signInForm.style.display="block";
-    signUpForm.style.display="none";
-})
+signUpButton.addEventListener('click', () => {
+  signInForm.style.display = "none";
+  signUpForm.style.display = "block";
+});
+
+signInButton.addEventListener('click', () => {
+  signInForm.style.display = "block";
+  signUpForm.style.display = "none";
+});
